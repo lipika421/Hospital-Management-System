13ab5e41248a37c7f7d54486896458d339dea968
Sai Lipika Korupolu <45463130+lipika421@users.noreply.github.com>
2021-03-31 22:01:23 +0530

    Add files via upload

---
+++ incl/footer.php
@@ -0,0 +1,13 @@
+<footer>         
+
+    <center>
+        <h4>For more info:</h4>
+        <p>HMS Hospitals</p>
+        <p>Contact Number:987654321</p>
+        <p>2nd Lane,Haritha Plaza,Kothapet,Hyderabad</p>
+    </center>
+
+
+</footer>
+</body>
+</html>
--- incl/header.php
+++ incl/header.php
@@ -1,1 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+      <title>HMS Hospitals</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="mystyle.css">
+    </head>
+    <body>
+       <div class="header">
+       
+          <div class="header-left">
+          
+          <a href="login.php">Home</a>
 
+          </div>
+
+          <div class = "header-right">
+          <?php
+                if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
+          ?>
+          <a href="login.php">Log in</a>
+                <?php }
+            else { ?>
+            <a href="logout.php">Log Out</a>
+          <?php }?>
+
+            </div>
+            
+      </div>
